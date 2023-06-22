55a6c75a95938dfe504817289b9457e36d77122c
monsef <monsefelbouichaoui@gmail.com>
2023-06-21 19:51:37 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,22 +9,15 @@
  * Return: 1 is greater than zero. 0 is zero 
  * -1 is less than zero.
  */
-int print_sign(int n)
-{
-if(n > 0)
-{
-_putchar(43);
-return(1);
-}
-else if(n < 0)
-{
-_putchar(45);
-return(-1);
-}
-else
-{
-_putchar(48);
-return(0);
-}
-_putchar('\n');
+int print_sign(int n) {
+    if (n > 0) {
+        printf("+");
+        return 1;
+    } else if (n == 0) {
+        printf("0");
+        return 0;
+    } else {
+        printf("-");
+        return -1;
+    }
 }
