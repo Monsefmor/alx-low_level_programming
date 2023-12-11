3c2cdd0846365ff374d17e543d376c6c42d3a59e
Monsefmor <monsefelbouichaoui@.com>
2023-12-11 12:43:54 -0800

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,15 +1,15 @@
+// This program prints the sum of the squares of the first n natural numbers
 #include <stdio.h>
 
-// Converts a binary string to an unsigned int
-unsigned int binary_to_uint(const char *b)
+int main()
 {
-  unsigned int num = 0;
-  if (!b) return 0;
-  while (*b)
-  {
-    num = num * 2 + (*b++ - '0');
-    if (num & ~1) return 0;
-  }
-  return num;
+    int n, sum = 0; // Declare variables
+    printf("Enter a positive integer: "); // Prompt user for input
+    scanf("%d", &n); // Read input
+    for (int i = 1; i <= n; i++) // Loop from 1 to n
+    {
+        sum += i * i; // Add square of i to sum
+    }
+    printf("The sum of squares is %d\n", sum); // Print result
+    return 0; // Exit program
 }
-
